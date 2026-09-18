101124250c339eeee5114431debc2dc10bfd16f0
isramiraj1443-ship-it <isramiraj1443@gmail.com>
2026-09-18 17:20:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 //  menghasilkan URL yang berbeda.
 // ============================================================
 window.APP_CONFIG = {
-  GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbxtoKcv4Aw320532U4WJcR_bPa3jGSKuEys3YKSiPxiozfYQEY8WU1KtohoRsd2NCnwEg/exec",
+  GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbwcDmkQMXeSyO2mnTvdAyxhzJY-djrJYvLPtqNS53n7P5Q0VRWHzEtPbZN93BZrZMHKXQ/exec",
   APP_NAME: "SIPINTAR",
   APP_TAGLINE: "SMP Negeri 26 Surakarta",
   APP_VERSION: "2.0.1"
