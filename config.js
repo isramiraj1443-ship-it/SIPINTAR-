197b83986d6a3a82f66e9db99462aaa72378d483
isramiraj1443-ship-it <isramiraj1443@gmail.com>
2026-09-21 16:52:40 +0700

    Update GAS_WEB_APP_URL in config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 //  Setelah Deploy code.gs, jika URL /exec BERUBAH, ganti di sini.
 // ============================================================
 window.APP_CONFIG = {
-  GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbz1Koo_sEORMAPBeFNfeoiujsCgRvB0t1Yijckk4RD4VrPbPBvqsdretfn1jVGT14FV4A/exec",
+  GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbzWMKpiFErd1P1b5WXv1S_s-T-UMZJBJRnEHITRgS_32Xrjc7wvdE1o3DJIJN78cTw2DA/exec",
   APP_NAME: "SIPINTAR",
   APP_TAGLINE: "SMP Negeri 26 Surakarta",
   APP_VERSION: "2.2.0"
